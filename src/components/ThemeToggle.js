8db694efeae84293d5ac72810a333b5130f99af3
Arjxx <arjunmultimedia@gmail.com>
2024-08-07 08:24:11 +0530

    070824

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import { useTheme } from '../ThemeContext';
 import { Switch, FormControlLabel } from '@mui/material';
+import { WbSunny, NightsStay } from '@mui/icons-material';
+
 
 const ThemeToggle = () => {
   const { toggleTheme, theme } = useTheme();
 
   return (
+
     <FormControlLabel
       control={<Switch checked={theme === 'dark'} onChange={toggleTheme} />}
-      label="Toggle Theme"
+      // control={theme === 'dark' ? <NightsStay /> : <WbSunny/> onChange={toggleTheme} }
+
+      label={theme === 'dark' ? <NightsStay /> : <WbSunny />}
     />
+
+
+
   );
 };
 
 export default ThemeToggle;
+
+
+// import React from 'react';
+// import { useTheme } from '../ThemeContext';
+// import { Switch, FormControlLabel, IconButton } from '@mui/material';
+// import { WbSunny, NightsStay } from '@mui/icons-material';
+
+// const ThemeToggle = () => {
+//   const { toggleTheme, theme } = useTheme();
+
+//   return (
+//     <FormControlLabel
+//       control={
+//         <IconButton onClick={toggleTheme}>
+//           {theme === 'dark' ? <NightsStay /> : <WbSunny />}
+//         </IconButton>
+//       }
+//       label=""
+//     />
+//   );
+// };
+
+// export default ThemeToggle;
